d3e619cbe8122ba3c12a03d07312bd15bff8fcaa
OlegBirykov <p_petro75@mail.ru>
2020-11-22 14:16:26 +0300

    Выполнено задание №1

--- src/task1/Portfolio.js
+++ src/task1/Portfolio.js
@@ -1,17 +1,31 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Toolbar from './Toolbar';
 import ProectList from './ProectList';
 
 function Portfolio(props) {
   const { proects, filters } = props;
-  
+
+  const [{ selected, proectList }, setSelected] = useState({ selected: "All", proectList: proects});
+
+  const selectFilter = (event) => {
+    setSelected(() => {
+      const filter = event.target.innerText;
+      return {
+        selected: filter,
+        proectList: proects.filter((proect) => filter === 'All' || filter === proect.category)
+      }
+    });
+  }
+
   return (
     <div className="task-1">
       <Toolbar 
         filters={filters}
-        selected="All" 
+        selected={selected} 
+        onSelectFilter={selectFilter}
       />
-      <ProectList proects={proects} />
+      <ProectList proects={proectList} />
     </div>
   );
 }
--- src/task1/Toolbar.js
+++ src/task1/Toolbar.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 
 function Toolbar(props) {
-  const { filters, selected } = props;
+  const { filters, selected, onSelectFilter } = props;
   
   return (
     <div className="t1-toolbar">
       {filters.map((filter) =>
-        <button key={filter} className={filter === selected ? "t1-button t1-selected" : "t1-button"}>{filter}</button>
+        <button key={filter} className={filter === selected ? "t1-button t1-selected" : "t1-button"} onClick={onSelectFilter}>
+          {filter}
+        </button>
       )}
     </div>
   );
@@ -14,7 +16,8 @@ function Toolbar(props) {
 
 Toolbar.propTypes = {
   filters: PropTypes.arrayOf(PropTypes.string).isRequired,
-  selected: PropTypes.string.isRequired
+  selected: PropTypes.string.isRequired,
+  onSelectFilter: PropTypes.func.isRequired
 }
 
 export default Toolbar;
